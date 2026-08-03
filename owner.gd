ae8838010a83014a6157f7957dd1e0d8ed068e82
crittergirl <dawnvoid@proton.me>
2026-08-03 13:03:49 -0400

    interpolate owner sprite to stop jittering

--- owner.gd
+++ owner.gd
@@ -1,5 +1,23 @@
 extends CharacterBody3D
 
+var input := Vector2.ZERO # keyboard movement input
+
+func _process(delta: float) -> void:
+	
+	# interpolate sprite position to physics position to stop jittering
+	%sprite.top_level = true
+	%sprite.global_position = lerp(%sprite.global_position, global_position + Vector3.UP * 0.325, 30 * delta)
+	
+	# rotate sprite according to movement for juice
+	var target_rotation := global_rotation.y # BUG? assumes owner is always vertical
+	if input.x < 0:
+		target_rotation += TAU * 0.1
+	elif input.x > 0:
+		target_rotation -= TAU * 0.1
+	
+	# interpolate sprite rotation to physics rotation to stop jittering
+	%sprite.global_rotation.y = lerp_angle(%sprite.global_rotation.y, target_rotation, 30 * delta)
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -7,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	# face the direction of the camera
 	global_rotation.y = %camera.global_rotation.y
 	
-	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_back").normalized()
+	input = Input.get_vector("move_left", "move_right", "move_forward", "move_back").normalized()
 	$charactermovement.move(input.rotated(-global_rotation.y), delta)
 	
 	move_and_slide()
