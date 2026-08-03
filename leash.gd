a69643e8328021b64c094bb79e4a7a287b9e4e69
crittergirl <dawnvoid@proton.me>
2026-08-03 17:37:10 -0400

    bweh

--- leash.gd
+++ leash.gd
@@ -5,12 +5,7 @@ func _ready() -> void:
 		curve.add_point(Vector3.ZERO)
 
 func _process(delta: float) -> void:
-	var a: Vector3 = %leashpointa.global_position
-	var b: Vector3 = %leashpointb.global_position
-	var mid := a + (b-a)/2 + Vector3.DOWN * 0.1
-	
-	curve.set_point_position(0, to_local(a))
-	curve.set_point_position(1, to_local(b))
-	
-	curve.set_point_out(0, to_local(mid) - to_local(a))
-	curve.set_point_in(1, to_local(mid) - to_local(a))
+	var a: Vector3 = to_local(%leashpointa.global_position)
+	var b: Vector3 = to_local(%leashpointb.global_position)
+	curve.set_point_position(0, a)
+	curve.set_point_position(1, b)
