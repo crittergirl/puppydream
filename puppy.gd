4401512825fd840d10c2f968cd70220554b56fe9
crittergirl <dawnvoid@proton.me>
2026-08-03 12:27:40 -0400

    make textures worse
    world textures are now x32 and npc textures are x64.
    make sure pixel art textures are set to 'uncompressed' when importing,
    or they'll have weird artifacts.

--- puppy.gd
+++ puppy.gd
@@ -9,7 +9,6 @@ func _process(delta: float) -> void:
 	if (-%camera.global_basis.z).angle_to(owner_direction):
 		var look_vector: Vector3 = (-%camera.global_basis.z).slerp(owner_direction, 20.0 * delta)
 		%camera.look_at(%camera.global_position + look_vector)
-	#%camera.look_at(%target.global_position)
 	
 	# camera bobbing + breathing
 	%camera.position.y = 0.1 + sin(camera_bobbing_t * 20) * 0.01
